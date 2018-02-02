0bdf2b4b607d463a7e86ba1b4edee2dc1fa87147
Nelreina <nelreina@gmail.com>
2018-02-02 19:13:29 -0400

    added express and proxy static images

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 require('dotenv').config();
+const express = require('express');
+const proxy = require('express-http-proxy');
+const path = require('path');
 
-console.info('App is running on port:', process.env.PORT);
+const app = express();
+const port = process.env.PORT;
+const baseImageUrl = process.env.BASE_IMAGE_URL;
+const localBaseImageUrl = express.static(
+  path.join(__dirname, '../public/images')
+);
+
+const proxyReqPathResolver = req => baseImageUrl + req.path;
+
+const proxyOptions = { proxyReqPathResolver };
+const proxyBaseImage = baseImageUrl
+  ? proxy(baseImageUrl, proxyOptions)
+  : localBaseImageUrl;
+
+app.use('/images', proxyBaseImage);
+
+app.listen(port, () => {
+  console.info('App is running on port:', port);
+});
