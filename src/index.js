68581a76b2ae3f2318297d52649245c436077ce7
Nelreina <nelreina@gmail.com>
2018-02-06 00:38:29 -0400

    added ping to service / check port is available before starting service

--- src/index.js
+++ src/index.js
@@ -3,22 +3,29 @@ const express = require('express');
 const proxy = require('express-http-proxy');
 const path = require('path');
 
-const app = express();
-const port = process.env.PORT;
-const baseImageUrl = process.env.BASE_IMAGE_URL;
-const localBaseImageUrl = express.static(
-  path.join(__dirname, '../public/images')
-);
+if (process.env.PORT) {
+  const app = express();
+  const port = process.env.PORT;
+  const baseImageUrl = process.env.BASE_IMAGE_URL;
+  const localBaseImageUrl = express.static(
+    path.join(__dirname, '../public/images')
+  );
 
-const proxyReqPathResolver = req => baseImageUrl + req.path;
+  const proxyReqPathResolver = req => baseImageUrl + req.path;
 
-const proxyOptions = { proxyReqPathResolver };
-const proxyBaseImage = baseImageUrl
-  ? proxy(baseImageUrl, proxyOptions)
-  : localBaseImageUrl;
+  const proxyOptions = { proxyReqPathResolver };
+  const proxyBaseImage = baseImageUrl
+    ? proxy(baseImageUrl, proxyOptions)
+    : localBaseImageUrl;
 
-app.use('/images', proxyBaseImage);
+  app.use('/images', proxyBaseImage);
 
-app.listen(port, () => {
-  console.info('App is running on port:', port);
-});
+  app.get('/ping', (req, res) => res.send('pong'));
+
+  app.listen(port, () => {
+    console.info('App is running on port:', port);
+  });
+} else {
+  console.error('Please Create a .env file');
+  process.exit(0);
+}
